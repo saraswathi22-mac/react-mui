137fbe5b9effe0976ecb716280c6a0ffa73d2a09
exit <saraswathi.mac@gmail.com>
2022-04-15 10:39:58 +0530

    Loading Button contd

--- src/components/35_LoadingButton.tsx
+++ src/components/35_LoadingButton.tsx
@@ -1,5 +1,7 @@
 import { Stack } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
+import { Save } from "@mui/icons-material";
+
 export const MuiLoadingButton = () => {
   return (
     <Stack spacing={2} direction="row">
@@ -12,6 +14,13 @@ export const MuiLoadingButton = () => {
       </LoadingButton>
       <LoadingButton loading variant="outlined" loadingIndicator="Loading...">
         Fetch data
+      </LoadingButton>
+      <LoadingButton
+        variant="outlined"
+        loadingPosition="start"
+        startIcon={<Save />}
+      >
+        Save
       </LoadingButton>
     </Stack>
   );
